551feb5a5280267c02e46e94a9c97493c6fc571f
r17_iNSANE <rakshith.pj@santec.com>
2024-09-23 16:38:27 +0900

    Add Project About

--- santec/__init__.py
+++ santec/__init__.py
@@ -1,4 +1,4 @@
-
+import datetime
 import santec.sts_process as STS
 from santec.get_address import GetAddress
 import santec.file_logging as file_logging
@@ -6,6 +6,18 @@ from santec.daq_device_class import SpuDevice
 from santec.mpm_instrument_class import MpmDevice
 from santec.tsl_instrument_class import TslDevice
 from santec.error_handing_class import instrument_error_strings, sts_process_error_strings
+
+
+# About
+__version__ = "2.6.2"
+__author__ = "Chentir MT"
+__organization__ = "Santec Holdings Corporation"
+__description__ = "Program to measure the Insertion Loss using the Swept Test System"
+__url__ = "https://github.com/santec-corporation/Santec_IL_STS"
+__date__ = "2024-09-23"
+__license__ = "GNU General Public License v3.0"
+__copyright__ = "Copyright 2022-{}, {}".format(datetime.date.today().year, __organization__)
+
 
 __all__ = [
     "STS",
