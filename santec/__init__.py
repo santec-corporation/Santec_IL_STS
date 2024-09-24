21c75b391fd31d723ef444113d1a2cd97b70327c
r17_iNSANE <rakshith.pj@santec.com>
2024-09-24 10:17:26 +0900

    Add program logging function.

--- santec/__init__.py
+++ santec/__init__.py
@@ -1,12 +1,5 @@
+import logging
 import datetime
-import santec.sts_process as STS
-from santec.get_address import GetAddress
-import santec.file_logging as file_logging
-from santec.daq_device_class import SpuDevice
-from santec.mpm_instrument_class import MpmDevice
-from santec.tsl_instrument_class import TslDevice
-from santec.error_handing_class import instrument_error_strings, sts_process_error_strings
-
 
 # About
 __version__ = "2.6.2"
@@ -19,6 +12,59 @@ __license__ = "GNU General Public License v3.0"
 __copyright__ = "Copyright 2022-{}, {}".format(datetime.date.today().year, __organization__)
 
 
+# Date and time for logging
+dt = datetime.datetime.now()
+dt = dt.strftime("%Y%m%d")
+
+PROJECT_NAME = "SANTEC_IL_STS"
+OUTPUT_LOGGER_NAME = f"output_{dt}.log"
+
+
+# Setup logging
+def setup_logging(level=logging.DEBUG):
+    setup_logger = logging.getLogger(PROJECT_NAME)
+    setup_logger.setLevel(level)
+
+    file_handler = logging.FileHandler(OUTPUT_LOGGER_NAME)
+    file_handler.setLevel(level)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    if setup_logger.hasHandlers():
+        setup_logger.handlers.clear()
+
+    setup_logger.addHandler(file_handler)
+    return setup_logger
+
+
+logger = setup_logging()
+logger.addHandler(logging.NullHandler())
+
+
+def log_to_screen(level=logging.DEBUG) -> None:
+    log_to_stream(None, level)
+
+
+def log_to_stream(stream_output, level=logging.DEBUG) -> None:
+    logger.setLevel(level)
+    ch = logging.StreamHandler(stream_output)
+    ch.setLevel(level)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+
+import santec.sts_process as STS
+from santec.get_address import GetAddress
+import santec.file_logging as file_logging
+from santec.daq_device_class import SpuDevice
+from santec.mpm_instrument_class import MpmDevice
+from santec.tsl_instrument_class import TslDevice
+from santec.error_handing_class import instrument_error_strings, sts_process_error_strings
+
 __all__ = [
     "STS",
     "TslDevice",
@@ -27,5 +73,6 @@ __all__ = [
     "GetAddress",
     "file_logging",
     "instrument_error_strings",
-    "sts_process_error_strings"
+    "sts_process_error_strings",
+    "logger"
 ]
