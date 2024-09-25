3676276f418e0d73594743fa136a04708dc1141e
r17_iNSANE <rakshith.pj@santec.com>
2024-09-25 10:46:06 +0900

    Restructure the DAQ device class.
    - Change method names to lower case.
    - Add logging.

--- santec/daq_device_class.py
+++ santec/daq_device_class.py
@@ -1,47 +1,42 @@
 # -*- coding: utf-8 -*-
 
 """
-Created on Thu Mar 24 20:04:16 2022
+DAQ Device Class.
 
-@author: chentir
-@organization: santec holdings corp.
+@organization: Santec Holdings Corp.
 """
 
-# Basic imports
 import os
 import clr
+from . import logger
 
 # Importing instrument error strings
 from santec.error_handing_class import instrument_error_strings
 
 # Adding Instrument DLL to the reference
 ROOT = str(os.path.dirname(__file__)) + '\\DLL\\'
+logger.info(f"Getting DLL path in DAQ device class, {ROOT}")
 # print(ROOT)    """ <-- uncomment in to check if the root was selected properly """
-
 PATH1 = 'InstrumentDLL'
-ans = clr.AddReference(ROOT + PATH1)
+ans = clr.AddReference(ROOT + PATH1)  # Add in santec.Instrument.DLL
+logger.info(f"Adding Instrument DLL to the root in DAQ device class, {ans}")
 # print(ans) #<-- comment in to check if the DLL was added properly
-
 
 # Importing SPU class from the DLL
 from Santec import SPU
 
 
 class SpuDevice:
-    """ DAQ board device class """
-
-    def __init__(self, device_name: str):
+    def __init__(self,
+                 device_name: str):
+        logger.info("Initializing Spu Instrument class.")
         self.__spu = SPU()
-        self.__deviceName = device_name
+        self._deviceName = device_name
+        logger.info(f"Spu Device details, Device Name: {device_name}")
 
-    def ConnectSPU(self):
+    def connect_spu(self):
         """
         Connects the DAQ board (SPU Connect).
-
-        Parameters
-        ----------
-        device_name : TYPE
-            DESCRIPTION.
 
         Raises
         ------
@@ -54,15 +49,17 @@ class SpuDevice:
             DESCRIPTION.
 
         """
-        self.__spu.DeviceName = str(self.__deviceName)
+        logger.info("Connect Spu device")
+        self.__spu.DeviceName = str(self._deviceName)
 
         errorcode, device_answer = self.__spu.Connect("")
         if errorcode != 0:
+            logger.critical("Spu instrument connection error ",
+                            str(errorcode) + ": " + instrument_error_strings(errorcode))
             raise Exception(str(errorcode) + ": " + instrument_error_strings(errorcode))
-
+        logger.info(f"Connected to Spu device, error string: {instrument_error_strings(errorcode)}")
         return instrument_error_strings(errorcode)
 
-    # SPU Set logging parameter
     def set_logging_parameters(self,
                                start_wavelength,
                                stop_wavelength,
@@ -79,44 +76,64 @@ class SpuDevice:
         Raises:
             Exception
         """
-
+        logger.info(f"Set SPU logging params: start_wavelength={start_wavelength}, stop_wavelength={stop_wavelength}, "
+                    f"sweep_speed={sweep_speed}, tsl_actual_step={tsl_actual_step}")
         errorcode = self.__spu.Set_Sampling_Parameter(start_wavelength,
                                                       stop_wavelength,
                                                       sweep_speed,
                                                       tsl_actual_step)
 
         if errorcode != 0:
+            logger.error("Error while setting SPU logging params, ",
+                         str(errorcode) + ": " + instrument_error_strings(errorcode))
             raise Exception(str(errorcode) + ": " + instrument_error_strings(errorcode))
-
+        logger.info(f"SPU logging params set. Error string:{instrument_error_strings(errorcode)}")
         return instrument_error_strings(errorcode)
 
     def sampling_start(self):
         """ Starts the SPU sampling """
+        logger.info("SPU sampling start")
         errorcode = self.__spu.Sampling_Start()
         if errorcode != 0:
+            logger.error("Error while SPU sampling start, ",
+                         str(errorcode) + ": " + instrument_error_strings(errorcode))
             raise RuntimeError(str(errorcode) + ": " + instrument_error_strings(errorcode))
+        logger.info("SPU sampling started.")
 
     def sampling_wait(self):
         """ SPU wait for sampling """
+        logger.info("SPU sampling wait")
         errorcode = self.__spu.Waiting_for_sampling()
         if errorcode != 0:
+            logger.error("Error while SPU sampling wait, ",
+                         str(errorcode) + ": " + instrument_error_strings(errorcode))
             raise RuntimeError(str(errorcode) + ": " + instrument_error_strings(errorcode))
+        logger.info("SPU sampling wait done.")
 
-    def get_sampling_raw(self):
+    def get_sampling_raw_data(self):
         """
         SPU get raw data
 
         returns:
         A list of trigger and monitor data
         """
+        logger.info("SPU get sampling raw data")
         errorcode, trigger, monitor = self.__spu.Get_Sampling_Rawdata(
             None, None)
         if errorcode != 0:
+            logger.error("Error while getting SPU sampling raw data, ",
+                         str(errorcode) + ": " + instrument_error_strings(errorcode))
             raise Exception(str(errorcode) + ": " + instrument_error_strings(errorcode))
+        logger.info(f"SPU sampling raw data acquired, data length: trigger={len(trigger)}, monitor={len(monitor)}")
         return trigger, monitor
 
-    def Disconnect(self):
+    def disconnect(self):
         """
-        Disconnects the spu device
+        Disconnects the SPU device
         """
-        self.__spu.DisConnect()
+        try:
+            self.__spu.DisConnect()
+            logger.info("SPU connection disconnected.")
+        except Exception as e:
+            logger.error(f"Error while disconnecting the SPU connection, {e}")
+        return None
